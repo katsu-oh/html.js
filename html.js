ca8b9eac1b16f1a1f17ce99c44ac0b277441ecb3
katsu-oh <98678838+katsu-oh@users.noreply.github.com>
2022-08-10 22:42:17 +0900

    Add files via upload

--- html.js
+++ html.js
@@ -1,4 +1,4 @@
-var LICENSE = 'html.js 1.4.1, ©2022 katsu-oh, MIT License: https://github.com/katsu-oh/html.js/blob/main/LICENSE';
+var LICENSE = 'html.js 1.4.2, ©2022 katsu-oh, MIT License: https://github.com/katsu-oh/html.js/blob/main/LICENSE';
 export {HTML, E};
 
 /*** (short) ***/
@@ -30,10 +30,8 @@ const HTML = function(...args){
 HTML.prototype.defineTag = function(name){
   Object.defineProperty(this, "$" + name, {configurable: true, enumerable: true,
     get(){
-      const newElement = document.createElement(name);
-      _content(this._current).appendChild(newElement);
       this._stack.push(this._current);
-      this._current = newElement;
+      this._current = _content(this._current).appendChild(document.createElement(name));
       this._call = _void;
       return this;
     }
@@ -95,7 +93,7 @@ HTML.prototype.defineStyle = function(name){
 "align-content,align-items,align-self,all,animation,animation-delay,animation-direction,animation-duration,animation-fill-mode,animation-iteration-count,animation-name,animation-play-state,animation-timing-function,azimuth,backface-visibility,background,background-attachment,background-clip,background-color,background-image,background-origin,background-position,background-repeat,background-size,border,border-bottom,border-bottom-color,border-bottom-left-radius,border-bottom-right-radius,border-bottom-style,border-bottom-width,border-collapse,border-color,border-image,border-image-outset,border-image-repeat,border-image-slice,border-image-source,border-image-width,border-left,border-left-color,border-left-style,border-left-width,border-radius,border-right,border-right-color,border-right-style,border-right-width,border-spacing,border-style,border-top,border-top-color,border-top-left-radius,border-top-right-radius,border-top-style,border-top-width,border-width,bottom,box-decoration-break,box-shadow,box-sizing,break-after,break-before,break-inside,caption-side,clear,clip,color,column-count,column-fill,column-gap,column-rule,column-rule-color,column-rule-style,column-rule-width,columns,column-span,column-width,Content,counter-increment,counter-reset,crop,cue,cue-after,cue-before,cursor,direction,display,elevation,empty-cells,fit,fit-position,flex,flex-basis,flex-direction,flex-flow,flex-grow,flex-shrink,flex-wrap,float,font,font-family,font-feature-settings,font-kerning,font-language-override,font-size,font-size-adjust,font-stretch,font-style,font-synthesis,font-variant,font-variant-alternates,font-variant-caps,font-variant-east-asian,font-variant-ligatures,font-variant-numeric,font-variant-position,font-weight,grid-cell,grid-column,grid-column-align,grid-columns,grid-column-sizing,grid-column-span,grid-flow,grid-row,grid-row-align,grid-rows,grid-row-sizing,grid-row-span,grid-template,hanging-punctuation,height,hyphens,icon,image-orientation,image-resolution,ime-mode,justify-content,left,letter-spacing,line-break,line-height,list-style,list-style-image,list-style-position,list-style-type,margin,margin-bottom,margin-left,margin-right,margin-top,marks,marquee-direction,marquee-play-count,marquee-speed,marquee-style,max-height,max-width,min-height,min-width,move-to,nav-down,nav-index,nav-left,nav-right,nav-up,object-fit,object-position,opacity,order,orphans,outline,outline-color,outline-offset,outline-style,outline-width,overflow,overflow-style,overflow-wrap,overflow-x,overflow-y,padding,padding-bottom,padding-left,padding-right,padding-top,page,page-break-after,page-break-before,page-break-inside,page-policy,pause,pause-after,pause-before,perspective,perspective-origin,pitch,pitch-range,play-during,position,quotes,resize,rest,rest-after,rest-before,richness,right,Size,speak,speak-as,speak-header,speak-numeral,speak-punctuation,speech-rate,stress,table-layout,tab-size,text-align,text-align-last,text-combine-horizontal,text-combine-mode,text-decoration,text-decoration-color,text-decoration-line,text-decoration-skip,text-decoration-style,text-emphasis,text-emphasis-color,text-emphasis-position,text-emphasis-style,text-indent,text-justify,text-orientation,text-overflow,text-shadow,text-transform,text-underline-position,top,transform,transform-origin,transform-style,transition,transition-delay,transition-duration,transition-property,transition-timing-function,unicode-bidi,vertical-align,visibility,voice-balance,voice-duration,voice-family,voice-pitch,voice-range,voice-rate,voice-stress,voice-volume,volume,white-space,widows,width,word-break,word-spacing,writing-mode,z-index".split(",").forEach(name => HTML.prototype.defineStyle(name));
 /*** text ***/
 HTML.prototype.T = function(...args){
-  _content(this._current).appendChild(document.createTextNode(_tagStr(args)));
+  _content(this._current).append(_tagStr(args));
   this._call = _void;
   return this;
 };
